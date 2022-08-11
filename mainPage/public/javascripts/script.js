078cf9b0e520ca77664be2a4c187e1763ebc7b86
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2022-08-11 00:01:38 -0400

    Avance con animacion de secciones

--- mainPage/public/javascripts/script.js
+++ mainPage/public/javascripts/script.js
@@ -40,12 +40,33 @@ tl.add({
   opacity: [0, 1],
   translateY: [30, 0],
   duration: 1000,
-}).add({
-  targets: '#sampleText1',
-  opacity: [1, 0],
-  translateY: [0, 30],
-  duration: 500,
 })
 
+// anime.timeline({
+//   easing: 'easeOutQuad',
+// }).add({
+//   targets: '.polymorph',
+//   points: [
+//     { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' },
+//     { value: '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369' },
+//     { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+//     { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+//     { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+//   ],
+//   easing: 'easeOutQuad',
+//   direction: 'alternate',
+//   loop: true,
+//   duration: 2000,
+// });
 
-// Animacion de links bonitos
+
+// Animacion del about
+var animation = anime({
+  targets: '.polygonalContainer',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'cubicBezier(.5, .05, .1, .3)',
+  duration: 1500,
+  autoplay: true,
+  delay: function(el, i) { return i * 250 },
+  loop: false,
+});
